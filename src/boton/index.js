ca8bbc681842efd55ec8eef69d91b48212c3beec
nicowave <c.nicofuentes@gmail.com>
2023-04-28 16:19:04 +0200

    first commit

---
+++ src/boton/index.js
@@ -0,0 +1,12 @@
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+export default function MyApp() {
+  return (
+    <div className="boton">
+      <h2>Welcome</h2>
+      <MyButton />
+    </div>
+  );
+}
